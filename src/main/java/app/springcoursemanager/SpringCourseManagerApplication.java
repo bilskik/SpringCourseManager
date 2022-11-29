2dd4c2a8dbedccf7f910d041b1a01b4f7e5541c0
bilskik <kbilski77@gmail.com>
2022-11-29 20:32:16 +0100

    init commit

---
+++ src/main/java/app/springcoursemanager/SpringCourseManagerApplication.java
@@ -0,0 +1,13 @@
+package app.springcoursemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCourseManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/springcoursemanager/SpringCourseManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.springcoursemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
